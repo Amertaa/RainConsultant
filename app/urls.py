27076bfc296e5f996f86b7a8be8cca0bf23320f7
Amertaa <btobdoo@gmail.com>
2024-07-17 19:17:22 +0800

    feat adding login page

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from app import views
 
 urlpatterns = [
     path(r'', views.home, name='home'),
-    
+    path('login', views.login, name='login')
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
+
+
+def login(request):
+    form = UserCreationForm()
+    return render(request, 'login.html')
